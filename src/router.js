3b49850c53a1e9ffc589639fffa6bb7f3a78918b
Roshan Connor <56798243+roshanconnor123@users.noreply.github.com>
2020-08-03 01:39:17 +0530

    Update router.js

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ const counting = {}
 const router = new Router()
 
 router.get('/api/gdurl/count', async ctx => {
-  if (!ROUTER_PASSKEY) return ctx.body = 'gd-utils 成功启动'
+  if (!ROUTER_PASSKEY) return ctx.body = 'gd-utils Successfully started'
   const { query, headers } = ctx.request
   let { fid, type, update, passkey } = query
   if (passkey !== ROUTER_PASSKEY) return ctx.body = 'invalid passkey'
@@ -41,10 +41,10 @@ router.get('/api/gdurl/count', async ctx => {
 
 router.post('/api/gdurl/tgbot', async ctx => {
   const { body } = ctx.request
-  console.log('ctx.ip', ctx.ip) // 可以只允许tg服务器的ip
+  console.log('ctx.ip', ctx.ip) // You can only allow the ip of the tg server
   console.log('tg message:', body)
   if (TG_IPLIST && !TG_IPLIST.includes(ctx.ip)) return ctx.body = 'invalid ip'
-  ctx.body = '' // 早点释放连接
+  ctx.body = '' // Release the connection early
   const message = body.message || body.edited_message
 
   const { callback_query } = body
@@ -53,22 +53,22 @@ router.post('/api/gdurl/tgbot', async ctx => {
     const chat_id = callback_query.from.id
     const [action, fid, target] = data.split(' ').filter(v => v)
     if (action === 'count') {
-      if (counting[fid]) return sm({ chat_id, text: fid + ' 正在统计，请稍等片刻' })
+      if (counting[fid]) return sm({ chat_id, text: fid + ' Counting, please wait a moment' })
       counting[fid] = true
       send_count({ fid, chat_id }).catch(err => {
         console.error(err)
-        sm({ chat_id, text: fid + ' 统计失败：' + err.message })
+        sm({ chat_id, text: fid + ' Stats failed：' + err.message })
       }).finally(() => {
         delete counting[fid]
       })
     } else if (action === 'copy') {
-      if (COPYING_FIDS[fid]) return sm({ chat_id, text: `正在处理 ${fid} 的复制命令` })
+      if (COPYING_FIDS[fid]) return sm({ chat_id, text: `Processing ${fid} Copy command` })
       COPYING_FIDS[fid] = true
       tg_copy({ fid, target: get_target_by_alias(target), chat_id }).then(task_id => {
-        task_id && sm({ chat_id, text: `开始复制，任务ID: ${task_id} 可输入 /task ${task_id} 查询进度` })
+        task_id && sm({ chat_id, text: `Start copying, task ID: ${task_id} can enter /task ${task_id} to show the progress` })
       }).finally(() => COPYING_FIDS[fid] = false)
     } else if (action === 'update') {
-      if (counting[fid]) return sm({ chat_id, text: fid + ' 正在统计，请稍等片刻' })
+      if (counting[fid]) return sm({ chat_id, text: fid + ' Counting, please wait a moment' })
       counting[fid] = true
       send_count({ fid, chat_id, update: true }).finally(() => {
         delete counting[fid]
@@ -89,47 +89,47 @@ router.post('/api/gdurl/tgbot', async ctx => {
   if (!chat_id || !text || !tg_whitelist.some(v => {
     v = String(v).toLowerCase()
     return v === username || v === user_id
-  })) return console.warn('异常请求')
+  })) return console.warn('Exception request')
 
   const fid = extract_fid(text) || extract_from_text(text)
   const no_fid_commands = ['/task', '/help', '/bm']
   if (!no_fid_commands.some(cmd => text.startsWith(cmd)) && !validate_fid(fid)) {
-    return sm({ chat_id, text: '未识别出分享ID' })
+    return sm({ chat_id, text: 'Shared ID not recognized' })
   }
   if (text.startsWith('/help')) return send_help(chat_id)
   if (text.startsWith('/bm')) {
     const [cmd, action, alias, target] = text.split(' ').map(v => v.trim())
     if (!action) return send_all_bookmarks(chat_id)
     if (action === 'set') {
-      if (!alias || !target) return sm({ chat_id, text: '别名和目标ID不能为空' })
-      if (alias.length > 24) return sm({ chat_id, text: '别名不要超过24个英文字符长度' })
-      if (!validate_fid(target)) return sm({ chat_id, text: '目标ID格式有误' })
+      if (!alias || !target) return sm({ chat_id, text: 'Alias and target ID cannot be empty' })
+      if (alias.length > 24) return sm({ chat_id, text: 'Alias should not exceed 24 English characters in length' })
+      if (!validate_fid(target)) return sm({ chat_id, text: 'Incorrect Destination ID format' })
       set_bookmark({ chat_id, alias, target })
     } else if (action === 'unset') {
-      if (!alias) return sm({ chat_id, text: '别名不能为空' })
+      if (!alias) return sm({ chat_id, text: 'Alias cannot be empty' })
       unset_bookmark({ chat_id, alias })
     } else {
       send_bm_help(chat_id)
     }
   } else if (text.startsWith('/count')) {
-    if (counting[fid]) return sm({ chat_id, text: fid + ' 正在统计，请稍等片刻' })
+    if (counting[fid]) return sm({ chat_id, text: fid + ' Counting, please wait a moment' })
     try {
       counting[fid] = true
       const update = text.endsWith(' -u')
       await send_count({ fid, chat_id, update })
     } catch (err) {
       console.error(err)
-      sm({ chat_id, text: fid + ' 统计失败：' + err.message })
+      sm({ chat_id, text: fid + ' Stats failed：' + err.message })
     } finally {
       delete counting[fid]
     }
   } else if (text.startsWith('/copy')) {
     let target = text.replace('/copy', '').replace(' -u', '').trim().split(' ').map(v => v.trim())[1]
     target = get_target_by_alias(target) || target
-    if (target && !validate_fid(target)) return sm({ chat_id, text: `目标ID ${target} 格式不正确` })
+    if (target && !validate_fid(target)) return sm({ chat_id, text: `Target ID ${target} is not in the correct format` })
     const update = text.endsWith(' -u')
     tg_copy({ fid, target, chat_id, update }).then(task_id => {
-      task_id && sm({ chat_id, text: `开始复制，任务ID: ${task_id} 可输入 /task ${task_id} 查询进度` })
+      task_id && sm({ chat_id, text: `Start copying, task ID: ${task_id} can enter /task ${task_id} to show the progress` })
     })
   } else if (text.startsWith('/task')) {
     let task_id = text.replace('/task', '').trim()
@@ -148,14 +148,14 @@ router.post('/api/gdurl/tgbot', async ctx => {
     task_id = parseInt(task_id)
     if (!task_id) {
       const running_tasks = db.prepare('select id from task where status=?').all('copying')
-      if (!running_tasks.length) return sm({ chat_id, text: '当前暂无运行中的任务' })
+      if (!running_tasks.length) return sm({ chat_id, text: 'There are currently no running tasks' })
       return running_tasks.forEach(v => send_task_info({ chat_id, task_id: v.id }).catch(console.error))
     }
     send_task_info({ task_id, chat_id }).catch(console.error)
   } else if (text.includes('drive.google.com/') || validate_fid(text)) {
     return send_choice({ fid: fid || text, chat_id })
   } else {
-    sm({ chat_id, text: '暂不支持此命令' })
+    sm({ chat_id, text: 'This command is not currently supported' })
   }
 })
 
