237de8e6d04e540ae61858fb2b29e6a026c3a181
Roshan Connor <56798243+roshanconnor123@users.noreply.github.com>
2020-08-03 01:22:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
-// 单次请求多少毫秒未响应以后超时（基准值，若连续超时则下次调整为上次的2倍）
+// How many milliseconds for a single request to time out（Reference value，If continuous timeout, it will be adjusted to twice the previous time）
 const TIMEOUT_BASE = 7000
-// 最大超时设置，比如某次请求，第一次7s超时，第二次14s，第三次28s，第四次56s，第五次不是112s而是60s，后续同理
+// Maximum timeout setting，For example, for a certain request, the first 7s timeout, the second 14s, the third 28s, the fourth 56s, the fifth is not 112s but 60s，Same follow-up
 const TIMEOUT_MAX = 60000
 
-const LOG_DELAY = 5000 // 日志输出时间间隔，单位毫秒
-const PAGE_SIZE = 1000 // 每次网络请求读取目录下的文件数，数值越大，越有可能超时，不得超过1000
+const LOG_DELAY = 5000 // Log output interval, in milliseconds
+const PAGE_SIZE = 1000 // Each network request to read the number of files in the directory, the larger the value, the more likely it will time out, and it should not exceed 1000
 
-const RETRY_LIMIT = 7 // 如果某次请求失败，允许其重试的最大次数
-const PARALLEL_LIMIT = 20 // 网络请求的并行数量，可根据网络环境调整
+const RETRY_LIMIT = 7 // The maximum number of retries allowed, If a request fails
+const PARALLEL_LIMIT = 20 // The number of parallel network requests can be adjusted according to the network environment
 
-const DEFAULT_TARGET = '' // 必填，拷贝默认目的地ID，如果不指定target，则会复制到此处，建议填写团队盘ID
+const DEFAULT_TARGET = '' // Required. Copy the default destination ID. If target is not specified, it will be copied here. It is recommended to fill in the TeamDrive ID
 
-const AUTH = { // 如果您拥有service account的json授权文件，可将其拷贝至 sa 目录中以代替 client_id/secret/refrest_token
+const AUTH = { // If you have the json authorization file of the service account, you can copy it to the sa directory instead of client_id/secret/refrest_token
   client_id: 'your_client_id',
   client_secret: 'your_client_secret',
   refresh_token: 'your_refrest_token',
-  expires: 0, // 可以留空
-  access_token: '', // 可以留空
-  tg_token: 'bot_token', // 你的 telegram robot 的 token，获取方法参见 https://core.telegram.org/bots#6-botfather
-  tg_whitelist: ['your_tg_username'] // 你的tg username(t.me/username)，bot只会执行这个列表里的用户所发送的指令
+  expires: 0, // Can be left blank
+  access_token: '', // Can be left blank
+  tg_token: 'bot_token', // Telegram bot token，Refer this to obtain it, https://core.telegram.org/bots#6-botfather
+  tg_whitelist: ['your_tg_username'] // Your tg username(t.me/username)，The bot will only execute commands sent by users in this list
 }
 
 module.exports = { AUTH, PARALLEL_LIMIT, RETRY_LIMIT, TIMEOUT_BASE, TIMEOUT_MAX, LOG_DELAY, PAGE_SIZE, DEFAULT_TARGET }
